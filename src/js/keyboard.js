88e7fda51e9986c28259ce115becb107f1f2f2db
rosie <hyemin52@naver.com>
2024-02-03 17:11:18 +0900

    코드 리팩토링
    1. document가 아닌, container 하위 앨리먼트로부터 탐색하므로 비용 절감
    2. 이벤트 핸들러 관리를 위해 분리

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,6 +1,7 @@
 export class Keyboard {
   #swichEl; // #(해시) => 프라이빗 변수는 클래스 외부에서 조회하거나 덮어쓸 수 없게된다.
   #fontSelectEl;
+  #containerEl;
 
   constructor() { // 키보드 클래스 인스턴스가 실행될 때 아래 메소드를 생성하고 초기화한다.
     this.#assignElement();
@@ -8,21 +9,26 @@ export class Keyboard {
   }
 
   #assignElement() {
-    this.#swichEl = document.getElementById("switch");
-    this.#fontSelectEl = document.getElementById("font");
+    this.#containerEl = document.getElementById("container");
+    this.#swichEl = this.#containerEl.querySelector("#switch");
+    this.#fontSelectEl = this.#containerEl.querySelector("#font");
   }
 
   #addEvent() {
     // console.log(this.#swichEl);
-    this.#swichEl.addEventListener("change", (event) => {
-      document.documentElement.setAttribute(
-        "theme",
-        event.target.checked ? "dark-mode" : "",
-      );
-    });
+    this.#swichEl.addEventListener("change", this.#onChangeTheme);
 
-    this.#fontSelectEl.addEventListener("change", (event) => {
-      document.body.style.fontFamily = event.target.value;
-    })
+    this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+  }
+
+  #onChangeTheme(event) {
+    document.documentElement.setAttribute(
+      "theme",
+      event.target.checked ? "dark-mode" : "",
+    );
+  }
+
+  #onChangeFont(event) {
+    document.body.style.fontFamily = event.target.value;
   }
 }
