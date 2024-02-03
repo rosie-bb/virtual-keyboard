71b28a136d1582acee88f4fec70c0af7049c4daa
rosie <hyemin52@naver.com>
2024-02-03 16:45:07 +0900

    주석 추가
    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
 import "../css/style.css";
-import { Keyboard } from "./keyboard";
-new Keyboard();
+import { Keyboard } from "./keyboard"; // 키보드 클래스 형성
+new Keyboard(); // 인스턴스 생성
--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,6 +1,7 @@
 export class Keyboard {
-  #swichEl; // 프라이빗변수
-  constructor() {
+  #swichEl; // #(해시) => 프라이빗 변수는 클래스 외부에서 조회하거나 덮어쓸 수 없게된다.
+  
+  constructor() { // 키보드 클래스 인스턴스가 실행될 때 아래 메소드를 생성하고 초기화한다.
     this.#assignElement();
     this.#addEvent();
   }
