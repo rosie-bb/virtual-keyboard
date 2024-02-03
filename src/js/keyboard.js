21b70120c9d100afd179dbbbc8a71bf8b1a7e4c8
rosie <hyemin52@naver.com>
2024-02-03 17:52:07 +0900

    키보드 이벤트 적용 및 이전 코드 리팩토링
    1. 한글 입력 방지 적용
    2. 영문 키보드 기능 리팩토링(이벤트 핸들러 분리)

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -3,6 +3,8 @@ export class Keyboard {
   #fontSelectEl;
   #containerEl;
   #keyboradEl;
+  #inputGroupEl;
+  #inputEl;
 
   constructor() { // 키보드 클래스 인스턴스가 실행될 때 아래 메소드를 생성하고 초기화한다.
     this.#assignElement();
@@ -14,6 +16,8 @@ export class Keyboard {
     this.#swichEl = this.#containerEl.querySelector("#switch");
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
     this.#keyboradEl = this.#containerEl.querySelector("#keyboard");
+    this.#inputGroupEl = this.#containerEl.querySelector("#input-group");
+    this.#inputEl = this.#inputGroupEl.querySelector("#input");
   }
 
   #addEvent() {
@@ -22,15 +26,29 @@ export class Keyboard {
 
     this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
 
-    document.addEventListener("keydown" , (event) => {
-      // 특수키 제외를 위해 ?.) Optional chaining 사용 => 에러가 발생하는 것 대신에 표현식의 리턴 값은 undefined
-      this.#keyboradEl.querySelector(`[data-code = ${event.code}]`)
-      ?.classList.add('active');
-    });
-    document.addEventListener("keyup" , (event) => {
-      this.#keyboradEl.querySelector(`[data-code = ${event.code}]`)
-      ?.classList.remove('active');
-    });
+    document.addEventListener("keydown" , this.#onKeyDown.bind(this));
+    document.addEventListener("keyup" , this.#onKeyUp.bind(this));
+
+    this.#inputEl.addEventListener("input", this.#onInput); // 한글을 공백으로(빈 string) 치환
+  }
+
+  #onKeyDown(event) {
+    // 한글 조합에 대한 정규식 사용으로 한글 입력 에러 표시
+    // console.log(event.key, /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key));
+    this.#inputGroupEl.classList.toggle("error", /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key));
+
+    // OS마다 특수키 위치가 다름에 따라 제외를 위해 ?.) Optional chaining 사용 => 에러가 발생하는 것 대신에 표현식의 리턴 값은 undefined
+    this.#keyboradEl.querySelector(`[data-code = ${event.code}]`)
+    ?.classList.add('active');
+  }
+
+  #onKeyUp(event) {
+    this.#keyboradEl.querySelector(`[data-code = ${event.code}]`)
+    ?.classList.remove('active');
+  } 
+
+  #onInput(event) {
+    event.target.value = event.target.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
   }
 
   #onChangeTheme(event) {
