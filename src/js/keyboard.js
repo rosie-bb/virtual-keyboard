9fddecda88b07fa054dc3342b6f85e10700ae04e
rosie <hyemin52@naver.com>
2024-02-03 16:50:39 +0900

    fontFamily 변경 기능 추가

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,6 +1,7 @@
 export class Keyboard {
   #swichEl; // #(해시) => 프라이빗 변수는 클래스 외부에서 조회하거나 덮어쓸 수 없게된다.
-  
+  #fontSelectEl;
+
   constructor() { // 키보드 클래스 인스턴스가 실행될 때 아래 메소드를 생성하고 초기화한다.
     this.#assignElement();
     this.#addEvent();
@@ -8,15 +9,20 @@ export class Keyboard {
 
   #assignElement() {
     this.#swichEl = document.getElementById("switch");
+    this.#fontSelectEl = document.getElementById("font");
   }
 
   #addEvent() {
-    console.log(this.#swichEl);
+    // console.log(this.#swichEl);
     this.#swichEl.addEventListener("change", (event) => {
       document.documentElement.setAttribute(
         "theme",
         event.target.checked ? "dark-mode" : "",
       );
     });
+
+    this.#fontSelectEl.addEventListener("change", (event) => {
+      document.body.style.fontFamily = event.target.value;
+    })
   }
 }
