761d6f1b2a78069a35ad2927e8bda9e514501f3e
rosie <hyemin52@naver.com>
2024-02-03 17:30:06 +0900

    키보드(영문) 이벤트 적용

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -2,6 +2,7 @@ export class Keyboard {
   #swichEl; // #(해시) => 프라이빗 변수는 클래스 외부에서 조회하거나 덮어쓸 수 없게된다.
   #fontSelectEl;
   #containerEl;
+  #keyboradEl;
 
   constructor() { // 키보드 클래스 인스턴스가 실행될 때 아래 메소드를 생성하고 초기화한다.
     this.#assignElement();
@@ -12,6 +13,7 @@ export class Keyboard {
     this.#containerEl = document.getElementById("container");
     this.#swichEl = this.#containerEl.querySelector("#switch");
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboradEl = this.#containerEl.querySelector("#keyboard");
   }
 
   #addEvent() {
@@ -19,6 +21,16 @@ export class Keyboard {
     this.#swichEl.addEventListener("change", this.#onChangeTheme);
 
     this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+
+    document.addEventListener("keydown" , (event) => {
+      // 특수키 제외를 위해 ?.) Optional chaining 사용 => 에러가 발생하는 것 대신에 표현식의 리턴 값은 undefined
+      this.#keyboradEl.querySelector(`[data-code = ${event.code}]`)
+      ?.classList.add('active');
+    });
+    document.addEventListener("keyup" , (event) => {
+      this.#keyboradEl.querySelector(`[data-code = ${event.code}]`)
+      ?.classList.remove('active');
+    });
   }
 
   #onChangeTheme(event) {
